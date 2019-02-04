e4db9cd74beffb32b8036f0fff87bfd5a656cd76
Gaxihyus <gaxyhs@hotmail.com>
2019-02-04 19:55:16 -0200

    asdwa

--- index.js
+++ index.js
@@ -268,11 +268,6 @@ const args = message.content.slice(prefix.length).trim().split(/ +/g);
          if (message.content.includes("DevAtomo") || message.content.includes("Atomo") || message.content.includes("atomo") || message.content.includes("devatomo")){
 
             let replies = ["Quem ta usando meu nome em vão ae?", message.author + ", falou de mim?", "o q tem eu?", "kkk eae men"];
-        
-        // yeet
-        if (message.content.includes("yeet") || message.content.includes("YEET") || message.content.includes("Yeet") || message.content.includes("YEET!")){
-
-            let replies = ["OOOOH BOOOOY", message.author + ", yeeet dude", "YEEEEET", "YEETTTTTTSAOGHDSIUGHSIUGYSUI"];    
 
             if(!message.content.includes(prefix)){
                 if(message.content.includes("gay") || message.content.includes("GAY") || message.content.includes("Gay") || message.content.includes("guei")){
