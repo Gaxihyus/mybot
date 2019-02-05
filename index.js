68c399cc5d450e9eb658f2d1f225856477156b41
Gaxihyus <gaxyhs@hotmail.com>
2019-02-05 10:36:36 -0200

    aaaa

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 const fs = require("fs");
 const math = require("mathjs");
 var client = new Discord.Client(); // Criar uma nova Client
-var giveMeAJoke = require('give-me-a-joke');
+
 
 
 // Configuração
 const config = require('./config.json');
 var prefix = config.prefix;
 var token = config.token; 
-
+var giveMeAJoke = require('give-me-a-joke');
 
 client.on("ready", function () { // Evento "quando a client estiver pronta/ligada" função:
 client.on('message', message=> {
